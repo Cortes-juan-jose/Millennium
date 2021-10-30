adc19e83ebf2bb4267d0cf9a8d0e97c65db6d97e
Cortes-Juan <cortes.juan.jose205@gmail.com>
2021-10-30 20:06:56 +0200

    Layout del activity_login.xml estructurado y diseñado para difinirse los campos del login

--- app/src/main/java/com/app/millennium/LoginActivity.kt
+++ app/src/main/java/com/app/millennium/LoginActivity.kt
@@ -2,10 +2,20 @@ package com.app.millennium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.millennium.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    
+        initUI()
+    }
+    
+    private fun initUI(){
+        
     }
 }
