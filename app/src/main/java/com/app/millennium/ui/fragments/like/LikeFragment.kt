c97607292d377ff8306cc900ad9de7f8b2c2ea91
Cortes-Juan <cortes.juan.jose205@gmail.com>
2021-11-13 15:45:45 +0100

    View Binding en los fragments ChatFragment.kt, HomeFragment.kt, LikeFragment.kt y ProfileFragment.kt

--- app/src/main/java/com/app/millennium/ui/fragments/like/LikeFragment.kt
+++ app/src/main/java/com/app/millennium/ui/fragments/like/LikeFragment.kt
@@ -1,19 +1,33 @@
 package com.app.millennium.ui.fragments.like
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.app.millennium.R
+import androidx.fragment.app.Fragment
+import com.app.millennium.databinding.FragmentLikeBinding
 
 class LikeFragment : Fragment() {
+
+    private var _binding : FragmentLikeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_like, container, false)
+        _binding = FragmentLikeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
