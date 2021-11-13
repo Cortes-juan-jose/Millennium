5a729a9a0503fce5e5309c19d206264846195786
Cortes-Juan <cortes.juan.jose205@gmail.com>
2021-11-13 15:52:30 +0100

    View Binding configurado en el fragment PostFragment.kt

--- app/src/main/java/com/app/millennium/ui/fragments/post/PostFragment.kt
+++ app/src/main/java/com/app/millennium/ui/fragments/post/PostFragment.kt
@@ -7,16 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import com.app.millennium.R
 import com.app.millennium.core.common.openActivity
+import com.app.millennium.databinding.FragmentPostBinding
 import com.app.millennium.ui.activities.post_product.PostProductActivity
 
 class PostFragment : Fragment() {
+
+    private var _binding: FragmentPostBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        _binding = FragmentPostBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     /**
@@ -25,5 +30,10 @@ class PostFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.openActivity<PostProductActivity> {  }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
