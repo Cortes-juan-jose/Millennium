644ce81a3d4172c96becce9cf5ab73d606345b88
Cortes-Juan <cortes.juan.jose205@gmail.com>
2021-11-15 20:44:57 +0100

    Activity PostProductActivity.kt preparada para desarrollar la lógica

--- app/src/main/java/com/app/millennium/ui/activities/post_product/PostProductActivity.kt
+++ app/src/main/java/com/app/millennium/ui/activities/post_product/PostProductActivity.kt
@@ -30,9 +30,31 @@ class PostProductActivity : AppCompatActivity() {
 
     private fun initUI(){
 
-        binding.ivBack.setOnClickListener {
-            onBackPressed()
-        }
+        /**
+         * Boton para ir hacia atras
+         */
+        binding.ivBack.setOnClickListener {onBackPressed()}
+
+        /**
+         * Seleccionar imagenes
+         */
+        configImages()
+
+        /**
+         * Campos selectores
+         */
+        configInputsSelectors()
+
+        /**
+         * Boton para publicar el producto
+         */
+        configPostProduct()
+    }
+
+    /**
+     * Metodo para publicar un producto
+     */
+    private fun configPostProduct() {
         binding.btnPost.setOnClickListener {
             this@PostProductActivity.reload()
             validarInputs()
@@ -44,5 +66,28 @@ class PostProductActivity : AppCompatActivity() {
      */
     private fun validarInputs() {
         toast("Validando inputs")
+    }
+
+    /**
+     * Metodo que configura los editext como botones de seleccion
+     */
+    private fun configInputsSelectors() {
+        binding.apply {
+            tietCategory.setOnClickListener { toast("Category") }
+            tietNegotiable.setOnClickListener { toast("Negotiable") }
+            tietProductStatus.setOnClickListener { toast("Product Status") }
+        }
+    }
+
+    /**
+     * Metodo que configura la selección de las imagenes
+     */
+    private fun configImages() {
+        binding.apply {
+            cvImgPost1.setOnClickListener { toast("IMG1") }
+            cvImgPost2.setOnClickListener { toast("IMG2") }
+            cvImgPost3.setOnClickListener { toast("IMG3") }
+            cvImgPost4.setOnClickListener { toast("IMG4") }
+        }
     }
 }
