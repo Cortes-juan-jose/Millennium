f000271e7a557843448bcca27a076e0d1235058e
Cortes-Juan <cortes.juan.jose205@gmail.com>
2021-11-21 19:24:47 +0100

    Se ha configurado el AppBarLayout del fragment_profile.xml y se ha configurado si el dispositivo está en modo oscuro o claro, dependiendo de esto entonces se configura con un drawable u otro el contentScrim

--- app/src/main/java/com/app/millennium/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/app/millennium/ui/fragments/profile/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package com.app.millennium.ui.fragments.profile
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.app.millennium.R
 import com.app.millennium.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -24,10 +27,23 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if (isThemeLight())
+            binding.ctlAppbar.contentScrim = context?.let { ContextCompat.getDrawable(it, R.drawable.toolbar_light) }
+        else
+            binding.ctlAppbar.contentScrim = context?.let { ContextCompat.getDrawable(it, R.drawable.toolbar_dark) }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun isThemeLight(): Boolean {
+        return when (context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {false}
+            Configuration.UI_MODE_NIGHT_NO -> {true}
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {false}
+            else -> {false}
+        }
     }
 }
